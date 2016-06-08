637ed064b7464b257fe46c199e21e834feb20b9b
Jonathan Hickford <jonathan.hickford@red-gate.com>
2016-06-08 16:05:36 +0100

    changed a number

--- Databases/DB2/Scripts/Stored Procedures/dbo.ShowaTweet.sql
+++ Databases/DB2/Scripts/Stored Procedures/dbo.ShowaTweet.sql
@@ -9,5 +9,5 @@ CREATE PROCEDURE [dbo].[ShowaTweet]
     @parameter_name AS INT
 -- WITH ENCRYPTION, RECOMPILE, EXECUTE AS CALLER|SELF|OWNER| 'user_name'
 AS
-    SELECT * FROM dbo.Tweets WHERE TweetID = 493
+    SELECT * FROM dbo.Tweets WHERE TweetID = 4923
 GO
