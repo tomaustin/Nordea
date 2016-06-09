1b24b89fbda4321d81c4920c7df02e9244f0ecc7
Jonathan Hickford <jonathan.hickford@red-gate.com>
2016-06-09 10:26:16 +0100

    added facebook

--- Databases/DB2/Scripts/Tables/dbo.Authors.sql
+++ Databases/DB2/Scripts/Tables/dbo.Authors.sql
@@ -2,7 +2,8 @@ CREATE TABLE [dbo].[Authors]
 (
 [AuthorID] [int] NOT NULL,
 [AuthorName] [nvarchar] (50) COLLATE Latin1_General_CI_AS NOT NULL,
-[AuthorTwitter] [nvarchar] (50) COLLATE Latin1_General_CI_AS NULL
+[AuthorTwitter] [nvarchar] (50) COLLATE Latin1_General_CI_AS NULL,
+[AuthorFacebook] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Authors] ADD CONSTRAINT [PK_Authors] PRIMARY KEY CLUSTERED  ([AuthorID]) ON [PRIMARY]
